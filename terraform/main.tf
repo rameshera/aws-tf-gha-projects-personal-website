2dae9c6b0f4fa0ff42f32070a80adcc85673c045
Ehsan Shekari <e.shekari@gmail.com>
2025-03-07 16:35:19 +0200

    use depend on

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,30 +10,6 @@ resource "aws_s3_bucket_website_configuration" "static_website_config" {
   }
 }
 
-# resource "aws_s3_bucket_policy" "static_site_policy" {
-#   bucket = aws_s3_bucket.static_site.id
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect    = "Allow"
-#         Principal = "*"
-#         Action    = "s3:GetObject"
-#         Resource  = "${aws_s3_bucket.static_site.arn}/*"
-#       }
-#     ]
-#   })
-# }
-
-# resource "aws_s3_bucket_public_access_block" "static_site_access" {
-#   bucket                  = aws_s3_bucket.static_site.id
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
-# }
-
-# Ensure Public Access Block is disabled BEFORE applying the bucket policy
 resource "aws_s3_bucket_public_access_block" "static_site_access" {
   bucket                  = aws_s3_bucket.static_site.id
   block_public_acls       = false
@@ -42,7 +18,6 @@ resource "aws_s3_bucket_public_access_block" "static_site_access" {
   restrict_public_buckets = false
 }
 
-# Bucket policy for public read access (depends on disabling public access block)
 resource "aws_s3_bucket_policy" "static_site_policy" {
   bucket = aws_s3_bucket.static_site.id
   policy = jsonencode({
@@ -57,5 +32,6 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
     ]
   })
 
-  depends_on = [aws_s3_bucket_public_access_block.static_site_access] # Ensure public access block is disabled first
+  depends_on = [ aws_s3_bucket_public_access_block.static_site_access ]
 }
+
