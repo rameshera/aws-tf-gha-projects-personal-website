219a97185335b8ef95ef107bf8d4d3c510a7c1ab
Ehsan Shekari <e.shekari@gmail.com>
2025-03-07 13:11:08 +0200

    creating a bucket from gha

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,13 +3,6 @@ provider "aws" {
 }
 
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws",
-      version = "5.76.0"
-    }
-  }
-  
   backend "s3" {
     bucket         = "tf-resources-gha"
     region         = "us-east-1"
