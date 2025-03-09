f8c1b1ea761f3ccc55b3f2327783b77e9544548e
Ehsan Shekari <e.shekari@gmail.com>
2025-03-09 16:47:14 +0200

    cloudfront distro

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,3 +76,81 @@ resource "aws_acm_certificate_validation" "ehsanshekari_cert_validation" {
   validation_record_fqdns = [for record in aws_route53_record.ehsanshekari_cert_validation : record.fqdn]
 }
 
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "oac-${aws_s3_bucket.static_site.bucket}"
+  description                       = "OAC for ${aws_s3_bucket.static_site.bucket}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.static_site.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = "S3-${aws_s3_bucket.static_site.bucket}"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CloudFront Distro for ${aws_s3_bucket.static_site.bucket}"
+  default_root_object = "index.html"
+
+  aliases = ["ehsanshekari.com", "www.ehsanshekari.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.static_site.bucket}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.ehsanshekari_cert.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  depends_on = [aws_acm_certificate_validation.ehsanshekari_cert_validation]
+}
+
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.static_site.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.static_site.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    ]
+  })
+}
