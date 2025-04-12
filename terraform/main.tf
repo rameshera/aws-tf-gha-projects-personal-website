a25e57f878b5ca40fa1a225de35b8799ff065b89
Ramesh E <adminrameshe@gmail.com>
2025-04-12 15:42:25 -0500

    upload files into s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,38 +2,38 @@
 #   bucket = var.bucket_name
 # }
 
-# resource "aws_s3_bucket_website_configuration" "static_website_config" {
-#   bucket = aws_s3_bucket.static_site.id
+resource "aws_s3_bucket_website_configuration" "static_website_config" {
+  bucket = aws_s3_bucket.static_site.id
 
-#   index_document {
-#     suffix = "index.html"
-#   }
-# }
+  index_document {
+    suffix = "index.html"
+  }
+}
 
-# resource "aws_s3_bucket_public_access_block" "static_site_access" {
-#   bucket                  = aws_s3_bucket.static_site.id
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_public_access_block" "static_site_access" {
+  bucket                  = aws_s3_bucket.static_site.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
-# # resource "aws_s3_bucket_policy" "static_site_policy" {
-# #   bucket = aws_s3_bucket.static_site.id
-# #   policy = jsonencode({
-# #     Version = "2012-10-17"
-# #     Statement = [
-# #       {
-# #         Effect    = "Allow"
-# #         Principal = "*"
-# #         Action    = "s3:GetObject"
-# #         Resource  = "${aws_s3_bucket.static_site.arn}/*"
-# #       }
-# #     ]
-# #   })
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.static_site.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
 
-# #   depends_on = [ aws_s3_bucket_public_access_block.static_site_access ]
-# # }
+  depends_on = [ aws_s3_bucket_public_access_block.static_site_access ]
+}
 
 # resource "aws_acm_certificate" "ehsanshekari_cert" {
 #   domain_name       = "ehsanshekari.com"
