a1510b211fb95f5b4a93e79dcd9bf6e373fb8c4d
Ramesh E <adminrameshe@gmail.com>
2025-04-12 15:33:34 -0500

    upload files into s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket" "static_site" {
-  bucket = var.bucket_name
-}
+# resource "aws_s3_bucket" "static_site" {
+#   bucket = var.bucket_name
+# }
 
 # resource "aws_s3_bucket_website_configuration" "static_website_config" {
 #   bucket = aws_s3_bucket.static_site.id
