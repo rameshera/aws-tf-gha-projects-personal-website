0546726e2737e34ebee6ca430a5c663815d0ef87
Ramesh E <adminrameshe@gmail.com>
2025-04-12 14:19:09 -0500

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "bucket_name" {
   type = string
   default = "statc-website-ramesh123456"
 }
+
+variable "bucket_name" {
+  description = "This is my bucket name"
+  type = string
+  default = "statc-website-ramesh12345678"
+}
